5cc3c7aa4974bbabd43385f1a71ccd9d31993c25
Josh Bailey <josh@vandervecken.com>
2024-05-19 10:56:25 +1200

    Revert "pin llvm-mos"
    This reverts commit 8712ca3383931de9df5f74fff13d9c0128957d9c.

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ export DEBIAN_FRONTEND=noninteractive && \
 echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
 sudo apt-get update && \
 sudo apt-get install -yq --no-install-recommends git vice sudo wget
-wget -q -O- https://github.com/llvm-mos/llvm-mos-sdk/releases/download/v15.3.0/llvm-mos-linux.tar.xz | tar -Jxvf - && sudo mv llvm-mos /usr/local && \
+wget -q -O- https://github.com/llvm-mos/llvm-mos-sdk/releases/latest/download/llvm-mos-linux.tar.xz | tar -Jxvf - && sudo mv llvm-mos /usr/local && \
 make
