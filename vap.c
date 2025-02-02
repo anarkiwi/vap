ad8c0e9e28e384b0ea923ee1322920b55f8b1115
Josh Bailey <josh@vandervecken.com>
2025-02-02 17:00:59 +1300

    move regid.h

--- vap.c
+++ vap.c
@@ -18,6 +18,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#include "regid.h"
 #include "vessel.h"
 #include <6502.h>
 #include <c64.h>
@@ -131,39 +132,6 @@ void (*datahandler)(void) = &noop;
 void (*stophandler)(void) = &noop;
 
 const unsigned char sidregs = 25;
-const unsigned char regidmap[] = {
-    0, // ID 0
-    1, // ID 1
-    2, // ID 2
-    3, // ID 3
-    5, // ID 4
-    6, // ID 5
-    7, // ID 6
-
-    8,  // ID 7
-    9,  // ID 8
-    10, // ID 9
-    12, // ID 10
-    13, // ID 11
-    14, // ID 12
-    15, // ID 13
-
-    16, // ID 14
-    17, // ID 15
-    19, // ID 16
-    20, // ID 17
-    21, // ID 18
-    22, // ID 19
-    23, // ID 20
-
-    24, // ID 21
-    4,  // ID 22
-    11, // ID 23
-    18, // ID 24
-    4,  // ID 25
-    11, // ID 26
-    18, // ID 27
-};
 
 volatile struct {
   unsigned char mask[4];
